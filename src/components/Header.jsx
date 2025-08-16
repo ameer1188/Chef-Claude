4e289423f6b15cd19574156155c6ad45c39de478
ameer suhail <ameershuailvp6@gmail.com>
2025-08-17 01:19:02 +0530

    install tailwindcss and update code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = (props) => {
     return(
-        <header>
-            <img className="chef-logo" src={props.img.src} alt={props.img.alt} />
-            <h1>Chef Claude</h1>
+        <header className="flex  bg-gradient-to-br from-purple-900 via-indigo-900 to-black text-white font-mono h-20 items-center justify-center w-screen">
+                <img className="w-10 h-10 rounded-full hover:scale-105" src={props.img.src} alt={props.img.alt} />
+                <h1 className="ml-3 text-4xl font-bold hover:scale-105">Chef Claude</h1>
         </header> 
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,19 +16,29 @@ const Main = () => {
     }
 
     return(
-        <main>
-           <form onSubmit={handleSubmit} className="add-incredient-form" action="">
+        <main className="items-center justify-center mx-30 my-2">
+           <form  onSubmit={handleSubmit} className="flex focus:within:scale-105 items-center my-10 mx-10" action="">
             <input 
                 type="text" 
                 placeholder="e.g oregano"
                 aria-label="add incredient"
                 name="ingredient"
+                className="w-5xl h-13 px-4 py-2  border border-gray-300 font-semibold 
+                focus:outline-none focus:ring-blue-500 rounded-tl-2xl rounded-bl-2xl "
             />
-            <button>Add ingredient</button>
+            <button
+            className=" w-1xl h-13 px-4 py-2 bg-indigo-800 text-white font-semibold 
+            rounded-tr-2xl rounded-br-2xl hover:bg-indigo-900 transition hover:scale-105 before:content-['+'] before:mr-2 "
+            >Add ingredient</button>
            </form>
-           <ul>
-            {ingredientsListItems}
-           </ul>
+           <div className=" h-80 p-25 bg-gradient-to-br from-purple-600 via-blue-700 to-black rounded-tr-2xl rounded-tl-2xl ">
+                <ul className="
+                backdrop-blur-md bg-white/20 shadow-lg max-w-xl max-h-fit py-10 text-white/80
+                my-auto mx-auto items- text-center justify-center
+                h-50 text-2xl font-mono  rounded-2xl">
+                    {ingredientsListItems}
+                </ul>
+           </div>
         </main>
     )
 }
